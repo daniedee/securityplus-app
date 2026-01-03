f3bafb5decd2ec1848c5a152be7f52bc15790b13
daniedee <68699454+daniedee@users.noreply.github.com>
2026-01-02 22:41:25 -0600

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE = "secplus-mvp-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./app.js",
+  "./manifest.json",
+  "./data/questions.json",
+  "./data/answers_1_25.json"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request))
+  );
+});
